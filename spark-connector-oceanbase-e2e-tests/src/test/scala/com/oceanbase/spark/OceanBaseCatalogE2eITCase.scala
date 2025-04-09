65ff3c4b01eda64a6f2396ed7adc2fd578f4b228
yuanoOo <zhaomingyuan.zmy@digital-engine.com>
2025-04-09 15:34:16 +0800

    Enhancement: Add direct-load test to catalog's e2e test.

--- spark-connector-oceanbase-e2e-tests/src/test/scala/com/oceanbase/spark/OceanBaseCatalogE2eITCase.scala
+++ spark-connector-oceanbase-e2e-tests/src/test/scala/com/oceanbase/spark/OceanBaseCatalogE2eITCase.scala
@@ -99,6 +99,58 @@ class OceanBaseCatalogE2eITCase extends SparkContainerTestEnvironment {
     matches = "^2\\.4\\.[0-9]$",
     disabledReason = "Catalog is only supported starting from Spark3."
   )
+  def testDirectLoad(): Unit = {
+    val sqlLines: util.List[String] = new util.ArrayList[String]
+    sqlLines.add(s"""
+                    |set spark.sql.catalog.ob=com.oceanbase.spark.catalog.OceanBaseCatalog;
+                    |set spark.sql.catalog.ob.url=$getJdbcUrlInContainer;
+                    |set spark.sql.catalog.ob.username=$getUsername;
+                    |set spark.sql.catalog.ob.password=$getPassword;
+                    |set `spark.sql.catalog.ob.schema-name`=$getSchemaName;
+                    |set `spark.sql.catalog.ob.direct-load.enabled`=true;
+                    |set `spark.sql.catalog.ob.direct-load.host`=$getHostInContainer;
+                    |set `spark.sql.catalog.ob.direct-load.rpc-port`=$getRpcPortInContainer;
+                    |set spark.sql.defaultCatalog=ob;
+                    |""".stripMargin)
+    sqlLines.add(
+      s"""
+         |INSERT INTO $getSchemaName.products VALUES
+         |(101, 'scooter', 'Small 2-wheel scooter', 3.14),
+         |(102, 'car battery', '12V car battery', 8.1),
+         |(103, '12-pack drill bits', '12-pack of drill bits with sizes ranging from #40 to #3', 0.8),
+         |(104, 'hammer', '12oz carpenter\\'s hammer', 0.75),
+         |(105, 'hammer', '14oz carpenter\\'s hammer', 0.875),
+         |(106, 'hammer', '16oz carpenter\\'s hammer', 1.0),
+         |(107, 'rocks', 'box of assorted rocks', 5.3),
+         |(108, 'jacket', 'water resistent black wind breaker', 0.1),
+         |(109, 'spare tire', '24 inch spare tire', 22.2);
+         |""".stripMargin)
+
+    sqlLines.add("select * from products limit 10;")
+
+    submitSQLJob(sqlLines, getResource(SINK_CONNECTOR_NAME), getResource(MYSQL_CONNECTOR_JAVA))
+
+    val expected: util.List[String] = util.Arrays.asList(
+      "101,scooter,Small 2-wheel scooter,3.1400000000",
+      "102,car battery,12V car battery,8.1000000000",
+      "103,12-pack drill bits,12-pack of drill bits with sizes ranging from #40 to #3,0.8000000000",
+      "104,hammer,12oz carpenter's hammer,0.7500000000",
+      "105,hammer,14oz carpenter's hammer,0.8750000000",
+      "106,hammer,16oz carpenter's hammer,1.0000000000",
+      "107,rocks,box of assorted rocks,5.3000000000",
+      "108,jacket,water resistent black wind breaker,0.1000000000",
+      "109,spare tire,24 inch spare tire,22.2000000000"
+    )
+    val actual: util.List[String] = queryTable("products")
+    assertEqualsInAnyOrder(expected, actual)
+  }
+
+  @Test
+  @DisabledIfSystemProperty(
+    named = "spark_version",
+    matches = "^2\\.4\\.[0-9]$",
+    disabledReason = "Catalog is only supported starting from Spark3."
+  )
   def testCatalogOp(): Unit = {
     val sqlLines: util.List[String] = new util.ArrayList[String]
     sqlLines.add(s"""
