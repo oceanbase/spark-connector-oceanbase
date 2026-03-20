17e91503facb18a134886a9c39413a885d6273ae
yuanoOo <zhaomingyuan.zmy@digital-engine.com>
2026-03-20 11:03:45 +0800

    fix: Resolve cgroup v2 compatibility issue in GitHub CI (#103)

--- spark-connector-oceanbase-e2e-tests/src/test/java/com/oceanbase/spark/utils/SparkContainerTestEnvironment.java
+++ spark-connector-oceanbase-e2e-tests/src/test/java/com/oceanbase/spark/utils/SparkContainerTestEnvironment.java
@@ -194,7 +194,10 @@ public abstract class SparkContainerTestEnvironment extends OceanBaseMySQLTestBa
         commands.add(jarStr);
         commands.add("-f /tmp/script.sql");
 
-        String command = String.format("/opt/spark/bin/spark-sql %s", String.join(" ", commands));
+        String command =
+                String.format(
+                        "JAVA_TOOL_OPTIONS='-XX:-UseContainerSupport' /opt/spark/bin/spark-sql %s",
+                        String.join(" ", commands));
         LOG.info(command);
         Container.ExecResult execResult = sparkContainer.execInContainer("bash", "-c", command);
         LOG.info(execResult.getStdout());
@@ -223,7 +226,8 @@ public abstract class SparkContainerTestEnvironment extends OceanBaseMySQLTestBa
 
         String command =
                 String.format(
-                        "timeout 2m /opt/spark/bin/spark-shell %s", String.join(" ", commands));
+                        "timeout 2m JAVA_TOOL_OPTIONS='-XX:-UseContainerSupport' /opt/spark/bin/spark-shell %s",
+                        String.join(" ", commands));
         LOG.info(command);
         Container.ExecResult execResult = sparkContainer.execInContainer("bash", "-c", command);
         LOG.info(execResult.getStdout());
